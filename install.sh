153dab2d773b92dcda55e10688b115360659928d
Teo2Peer <teo2din@gmail.com>
2024-08-24 03:31:28 +0200

    feat: Update display configuration and add lazy loading for themes and images
    This commit updates the display configuration in the `config.toml` file. The `invert` setting is changed to `true` and the `driver` is set to `only_web`. Additionally, lazy loading is implemented for themes in the `setup.py` file and for images in the `main.py` file. This improves the performance of the application by loading themes and images asynchronously.

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install python3-dev python3-pip libffi-dev libssl-dev -y
+sudo apt-get install python3-numpy -y
+sudo apt-get install libopenjp2-7 -y
+sudo apt install libtiff5 -y
+sudo apt-get install ifstat -y
+sudo apt-get install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 -y
+
+sudo pip3 install --upgrade pip
+sudo pip3 install -r requirements.txt
