bf697b919dee9acd0a15cbfd35b9dcdf1a0d5115
Teo2Peer <teo2din@gmail.com>
2024-08-25 18:10:27 +0200

    feat: Add helpers module for directory filtering and option selection
    This commit adds a new module `helpers.py` which contains classes and functions for directory filtering and option selection. The `DirFilter` class provides methods to filter directories and files in a given directory, while the `Selector` class allows users to select an option from a list. This addition enhances the functionality of the PiPerW application by providing reusable and convenient utilities for working with directories and user selections.

---
+++ PiPerW/helpers.py
@@ -0,0 +1,140 @@
+import toml
+import os
+import sys
+import inspect
+import threading
+from PiPerW.utils.Logging import Logging
+
+
+
+class BCOLORS:
+    '''
+    Colors for the terminal
+    '''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+
+#---------------------------
+#     Filter dir
+#---------------------------
+class DirFilter:
+    
+    def __init__(self, dir):
+        '''
+        Filter directories and files
+        
+        :param dir: str: Directory to filter
+        '''
+        self.dir = dir
+    
+    def files(self):
+        '''
+        Get all files in a directory
+        
+        :return: list: List of files
+        '''
+        return [f for f in os.listdir(self.dir) if os.path.isfile(os.path.join(self.dir, f))]
+    
+    def folders(self):
+        '''
+        Get all directories in a directory
+        
+        :return: list: List of directories
+        '''
+        return [f for f in os.listdir(self.dir) if os.path.isdir(os.path.join(self.dir, f))]
+    
+
+#---------------------------
+#     SECTION HEADER
+#---------------------------
+
+class Selector:
+    def __init__(self, options, message = "Select an option"):
+        '''
+        Select an option from a list
+        
+        :param options: list: List of options
+        :param message: str: Message to display
+        '''
+        self.options = options
+        self.message = message
+        
+    def select(self):
+        '''
+        Select an option from a list
+        
+        :return: str: Selected option
+        '''
+        
+        print(f"\n{BCOLORS.OKCYAN}{self.message}{BCOLORS.ENDC}")
+        for i, option in enumerate(self.options):
+            print(f"{BCOLORS.OKGREEN}{i+1}{BCOLORS.ENDC}. {option}")
+        
+        while True:
+            try:
+                choice = int(input(BCOLORS.OKBLUE+  "Option: "+BCOLORS.ENDC))
+                if choice > 0 and choice <= len(self.options):
+                    return self.options[choice-1]
+                else:
+                    print("Invalid option")
+            except ValueError:
+                print("Invalid option")
+
+#---------------------------
+# Custom trheading class for exception handling
+#---------------------------
+class WThread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, daemon=None):
+        threading.Thread.__init__(self, group=group, target=target, name=name, args=args, kwargs=kwargs, daemon=daemon)
+        self.exc = None
+        self._stop_event = threading.Event()
+
+
+    def run(self):
+        try:
+            if self._target:
+                self._target(*self._args, **self._kwargs)
+        except Exception as e:
+            self.exc = e
+
+    def join(self, timeout=None):
+        threading.Thread.join(self, timeout)
+        if self.exc:
+            raise self.exc
+    
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
+
+
+
+#---------------------------
+#     Congiguration file
+#---------------------------
+Config = {}
+Config = toml.load('Config.toml') 
+
+def save_config():
+    '''
+    Save the Configuration file
+    '''
+    with open('Config.toml', 'w') as f:
+        toml.dump(Config, f)
+        
+
+#---------------------------
+#     Logging
+#---------------------------
+Log = Logging(Config['general']['debug'], os.path.dirname(os.path.abspath(__file__)))
+Log.info("Helpers loaded")
