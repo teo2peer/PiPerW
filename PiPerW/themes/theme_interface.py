6ccf4a415f6e6a9b7294d9582fc6c983169c3547
Teo2Peer <teo2din@gmail.com>
2024-08-26 15:36:46 +0200

    feat: Add ThemeInterface class
    This commit adds the `ThemeInterface` class to the `theme_interface.py` file. The `ThemeInterface` class represents an interface for themes in the PiPerW application. It has attributes for name, frame index, and max frames, as well as methods for getting the next frame and retrieving all frames for a specific theme. This addition enhances the PiPerW application by providing a standardized interface for themes and frame handling.

---
+++ PiPerW/themes/theme_interface.py
@@ -0,0 +1,46 @@
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+
+class ThemeInterface:
+    def __init__(self):
+        '''
+        Initialize the theme
+        
+        :param width: int: Width of the display
+        :param height: int: Height of the display
+        '''
+        self.name = "base_theme"
+        self.frame_index = 0
+        self.max_frames = 0
+
+        self.frameArray = [] # Array to store the frames from frames/{theme} folder
+        
+        
+    def next_frame(self):
+        '''
+        Loop through the frames
+        
+        :return: Image: The next frame
+        '''
+        if self.frame_index == self.max_frames:
+            self.frame_index = 0
+        
+        frame = self.frameArray[self.frame_index]
+        self.frame_index += 1
+        return frame
+    
+    def get_frames(self, theme):
+        '''
+        Get all the frames from the frames/{theme} folder
+        
+        :param theme: str: The theme to get the frames from
+        :return: list: List of frames
+        '''
+        frames = []
+        for i in range(0, 100):
+            try:
+                frame = Image.open('PiPerW/themes/{}/frames/{}.bmp'.format(theme, i))
+                frames.append(frame)
+            except:
+                self.max_frames = i
+                break
+        return frames
