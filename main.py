36467b2cc176486b6ec05b962929492e42a5e332
Teo2Peer <teo2din@gmail.com>
2024-08-26 15:35:39 +0200

    feat: Add timeout configuration for display driver

--- main.py
+++ main.py
@@ -1,186 +1,157 @@
-# -_- coding: utf-8 -_-
+# -*- coding: utf-8 -*-
 # Author: Teo2Peer
 # Date: 2021
 # Description: PiPerW 
 
 from PiPerW.helpers import Config, WThread, Log
 from PiPerW.utils.Menu import MenuFolder
-import PiPerW.utils.Logging
-import sys, os, time
+from PiPerW.display import Display
 import importlib
 import multiprocessing
-
+import os
+import sys
+import time
 
 last_activity = 0
+Display = Display()
 
 def first_run():
-    
     Log.warning("First run detected")
-    # improt setup script
-    setup = importlib.import_module("PiPerW.setup")
-    Log.info("Setup script imported")
-    
-    # run setup script
     try:
-        t = WThread(target=setup.install)
-        t.start()
-        t.join()
+        setup = importlib.import_module("PiPerW.setup")
+        Log.info("Setup script imported")
+        with WThread(target=setup.install) as t:
+            t.start()
+            t.join()
         
-        # install packages
+        # Install packages
         Log.info("Installing packages")
-        os.system("sudo apt update")    
-        os.system("sudo apt upgrade -y")
+        os.system("sudo apt update && sudo apt upgrade -y")
         os.system("./install.sh")
         
-        # restart program
+        # Restart program
         Log.info("Restarting PiPerW")
         os.system("sudo python3 main.py")
         
     except Exception as e:
-        Log.exception("Error running setup script: {}".format(e))
+        Log.exception(f"Error running setup script: {e}")
         sys.exit(1)
-    
 
-def init():
-    
-    Log.info("Initializing PiPerW")
-    Log.info("Config file loaded")
-    
-    if Config['general']['first_run']:
-        first_run()
-
-    #---------------------------
-    #     Init display
-    #---------------------------
-    Log.warning("Init display driver")
+def initialize_display():
     try:
-        display = importlib.import_module("PiPerW.display.{}".format(Config['display']['driver'])).Driver
-        display.init()
+        Display.init()
     except Exception as e:
-        Log.exception("Error initializing display driver: {}".format(e))
+        Log.exception(f"Error initializing display driver: {e}")
         sys.exit(1)
-    
-    display.clear()
-    display.progress_bar(20, "Loading PiPerW", True)
-   
-    
-    #---------------------------
-    #     Init web server
-    #---------------------------
+    return Display
+
+def initialize_web_server():
     if Config['display_cast']['default']:
-        Log.warning("Init web server")
+        Log.warning("Initializing web server")
         try:
             web = importlib.import_module("PiPerW.utils.Web").web_server
             multiprocessing.Process(target=web.run).start()
         except Exception as e:
-            Log.exception("Error initializing web server: {}".format(e))
+            Log.exception(f"Error initializing web server: {e}")
             sys.exit(1)
-    
-    display.progress_bar(40, "Loading PiPerW", True)
-    
-    #---------------------------
-    #     Pheripherals
-    #---------------------------
-    
-    Log.warning("Init pheripherals")
+
+def initialize_peripherals():
+    Log.warning("Initializing peripherals")
     try:
         pheripherals = importlib.import_module("PiPerW.pheripherals").Pheripherals()
     except Exception as e:
-        Log.exception("Error initializing pheripherals: {}".format(e))
+        Log.exception(f"Error initializing peripherals: {e}")
         sys.exit(1)
-    
-    
-    display.progress_bar(60, "PiPerW")
+    return pheripherals
 
-    # wait for pheripherals to start
-    # pheripherals_thread.join()
-        
+def init():
+    Log.info("Initializing PiPerW")
+
+    if Config['general']['first_run']:
+        first_run()
+
+    initialize_display()
+    Display.clear()
+    Display.progress_bar(20, "Loading PiPerW", True)
+
+    initialize_web_server()
+    Display.progress_bar(40, "Loading PiPerW", True)
+
+    pheripherals = initialize_peripherals()
+    Display.progress_bar(60, "PiPerW")
+
     Log.info("PiPerW initialized")
-    menu = MenuFolder(display.width, display.height, "apps")
-    display.draw(menu.generate())
-    
-    # time.sleep(1) # This solvs the issue of the first key press not being registered? 
-    # Maybe a mutex is needed?
-    
-    while True:
-        check_last_activity(pheripherals, display)
-        key = pheripherals.get_key()
-        # print(key)   # FIXME: Issue when not printing key
+    menu = MenuFolder(Display.width, Display.height, "apps")
+    Display.draw(menu.generate())
 
-        if(key == "up"):
-            menu.previous()
-            display.draw(menu.generate())
-        elif(key == "down"):
-            menu.next()
-            display.draw(menu.generate())
-        elif(key == "select"):
-            app_finder(menu.get_selected(), display, pheripherals)
-            display.draw(menu.generate())
-        elif(key == "back"):
+    while True:
+        if check_last_activity(pheripherals):
+            Display.draw(menu.generate())
+        
+        key = pheripherals.get_key()
+        handle_key_press(key, menu, pheripherals)
+        if key == "back":
             break
-        
-        
-        
-        key = None
-        
-    
-    
-def check_last_activity(pheripherals, display):
+
+def check_last_activity(pheripherals):
     global last_activity
     if time.time() - pheripherals.timestamp > Config['display']['timeout']:
         Log.warning("Screen timeout")
-        display.splashscreen()
-        pheripherals.await_key()
+        Display.splashscreen()
+        pheripherals.await_any_key_press()
         Log.info("Screen wakeup")
         last_activity = time.time()
-        display.stop_animation()
+        Display.stop_animation()
         return True
     return False
 
-def app_finder(folder, display, pheripherals):
-    apps = []
-    apps_menu = MenuFolder(display.width, display.height, "apps/{}".format(folder))
-    display.draw(apps_menu.generate())
+def handle_key_press(key, menu, pheripherals):
+    if key in ("up", "down", "select"):
+        handle_menu_navigation(key, menu, pheripherals)
+    elif key == "back":
+        return
+
+def handle_menu_navigation(key, menu, pheripherals):
+    if key == "up":
+        menu.previous()
+    elif key == "down":
+        menu.next()
+    elif key == "select":
+        app_finder(menu.get_selected(), pheripherals)
+    Display.draw(menu.generate())
+
+def app_finder(folder, pheripherals):
+    apps_menu = MenuFolder(Display.width, Display.height, f"apps/{folder}")
+    Display.draw(apps_menu.generate())
     while True:
         key = pheripherals.get_key()
-        if(key == "up"):
-            apps_menu.previous()
-            display.draw(apps_menu.generate())
-        elif(key == "down"):
-            apps_menu.next()
-            display.draw(apps_menu.generate())
-        elif(key == "select"):
-            display.text("Loading\n{}".format(apps_menu.get_selected()))
-        elif(key == "back"):
+        if key in ("up", "down"):
+            handle_menu_navigation(key, apps_menu, pheripherals)
+        elif key == "select":
+            execute_app(apps_menu.get_selected(), folder, pheripherals)
+            Display.draw(apps_menu.generate())
+        elif key == "back":
             break
-        
-        key = None
 
-def execute_app(app):
-    importlib.import_module("PiPerW.apps.{}".format(app)).app
-    
+def execute_app(app, folder, pheripherals):
     try:
-        app = importlib.import_module("PiPerW.apps.{}".format(app)).app
-        WThread(target=app.run).start()
+        app_module = importlib.import_module("apps.{}.{}".format(folder,app)).App()
+
+        t = WThread(target=app_module.run)
+        t.start()
+        t.join()
     except Exception as e:
-        Log.exception("Error running app {}: {}".format(app,e))
-        display.text("Error running app {}: ".format(app))
-        pheripherals.await_key()
-        sys.exit(1)
-
-
+        Log.exception(f"App crashed\n{app}: {e}")
+        Display.text(f"Error running app\n{app}\n\nLog in output.log")
+        pheripherals.await_any_key_press()
+        
 
 if __name__ == "__main__":
     try:
         init()
-    # check for ctrl+c
     except KeyboardInterrupt:
         Log.info("Exiting PiPerW")
-        
-        # stop all processes
-        # multiprocessing.Process.terminate()
         sys.exit(0)
     except Exception as e:
-        Log.exception("Error initializing PiPerW: {}".format(e))
+        Log.exception(f"Error initializing PiPerW: {e}")
         sys.exit(1)
-        
