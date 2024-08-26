2eec2fdaf5e45f64a9eac9f978fd2f3880555936
Teo2Peer <teo2din@gmail.com>
2024-08-27 01:23:58 +0200

    minor ajust

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 sudo apt-get update
 sudo apt-get install python3-dev python3-pip libffi-dev libssl-dev -y
 sudo apt-get install python3-numpy -y
-sudo apt-get install libopenjp2-7 -y
-sudo apt install libtiff5 -y
 sudo apt-get install ifstat -y
-sudo apt-get install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5  python3-dev python3-smbus i2c-tools python3-pip python3-venv -y
+sudo apt-get install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 libatlas-base-dev python3-dev python3-smbus i2c-tools python3-pip python3-venv fonts-dejavu -y
 
 sudo pip3 install --upgrade pip
 sudo pip3 install -r requirements.txt
