c195eea6a6e85983c490e4a8aa495b80ae4b0f5d
teo2peer <teo2din@gmail.com>
2024-08-26 17:40:33 +0200

    chore: Update install.sh to include additional Python dependencies
    This commit updates the install.sh script to include additional Python dependencies required for the project. The new dependencies include python3-dev, python3-smbus, i2c-tools, and python3-venv. This update ensures that all necessary packages are installed for proper functionality.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get install python3-numpy -y
 sudo apt-get install libopenjp2-7 -y
 sudo apt install libtiff5 -y
 sudo apt-get install ifstat -y
-sudo apt-get install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 -y
+sudo apt-get install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5  python3-dev python3-smbus i2c-tools python3-pip python3-venv -y
 
 sudo pip3 install --upgrade pip
 sudo pip3 install -r requirements.txt
