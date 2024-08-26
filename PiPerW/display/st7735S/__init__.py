c70868150eb1ce2b67d98486543e743c2f9992f3
Teo2Peer <teo2din@gmail.com>
2024-08-27 01:23:43 +0200

    refactor: Add support for ST7735S display module

---
+++ PiPerW/display/st7735S/__init__.py
@@ -0,0 +1,89 @@
+# -*- coding:utf-8 -*-
+from PiPerW.display.display_interface import DisplayInterface
+import digitalio
+import board
+from PIL import Image, ImageDraw
+import sys, time, subprocess, os, string
+
+from adafruit_rgb_display import st7735
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import RPi.GPIO as GPIO
+
+
+
+GPIO.setmode(GPIO.BCM) 
+
+
+class Driver(DisplayInterface):
+    
+    
+
+    def __init__(self):
+        
+        
+       
+
+        
+        
+        self.item_height = 32
+        self.horizontal_margin = 10
+        self.vertical_margin = 10
+        
+        # Some constants
+        self.SCREEN_LINES = 6
+        self.CHAR_WIDTH = 19
+        
+        
+        
+        #GPIO define
+        self.cs_pin = digitalio.DigitalInOut(board.CE0)
+        self.dc_pin = digitalio.DigitalInOut(board.D25)
+        self.reset_pin = digitalio.DigitalInOut(board.D24)
+        
+        self.BAUDRATE = 24000000
+        
+        self.spi = board.SPI()
+        self.device = st7735.ST7735(
+            self.spi, 
+            cs=self.cs_pin, 
+            dc=self.dc_pin, 
+            rst=self.reset_pin, 
+            baudrate=self.BAUDRATE
+        )
+        
+        self.width = device.width
+        self.height = device.height
+        
+        super().__init__(self.width, self.height, self.item_height, self.horizontal_margin, self.vertical_margin)
+        
+
+        
+    def command(self, cmd):
+        """Send a command to the display."""
+        self.device.write(cmd)
+        
+        
+
+    def reset(self):
+        """Reset the display."""
+        self.device.reset()
+        time.sleep(0.01)
+
+    
+
+    def show(self, image):
+        """Show an image on the display."""
+        
+        self.device.image(image)
+            
+        
+
+    def clear(self):
+        """Clear the display."""
+        self.device.fill(0)
+        self.device.show()
+
