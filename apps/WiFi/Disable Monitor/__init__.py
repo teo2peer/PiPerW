0d8fbfee6ada5cfa1690d3f924ec956bb2e841af
Teo2Peer <teo2din@gmail.com>
2024-08-27 11:25:33 +0200

    disable monitor mode app

---
+++ apps/WiFi/Disable Monitor/__init__.py
@@ -0,0 +1,46 @@
+# sudo airmon-ng start wlan0
+# iwconfig check if monitor mode is enabled
+
+# get all bssid
+# sudo airodump-ng wlan0mon --output-format csv -w /tmp/scan
+
+# disconnect all clients
+# sudo aireplay-ng --deauth 0 -a BSSID wlan0mon
+
+from PiPerW.apps.app_interface import AppInterface
+from PiPerW.pheripherals import Pheripherals
+from PiPerW.display import Display
+from PiPerW.helpers import Log
+import os, time
+
+display = Display()
+pheripherals = Pheripherals()
+
+
+
+class App(AppInterface):
+    
+    name = "Wifi Jammer"
+    version = "0.1"
+    
+    def __init__(self):
+        super().__init__(self.name, self.version)
+        self.menu = None
+        
+    def init(self):
+        Log.warning("Disabling Monitor Mode")
+        display.text("Disabling Monitor Mode")
+        time.sleep(1)
+        
+        
+        os.system("sudo airmon-ng stop wlan0mon")
+            
+        Log.info("Monitor mode disabled")
+        display.text("Monitor mode disabled")
+        pheripherals.await_any_key_press()
+        
+        
+    
+
+    def run(self):
+        self.init()
