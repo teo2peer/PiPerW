e2f9d5fc7b977748905b6a5c5e9d8f2e92573c05
Teo2Peer <teo2din@gmail.com>
2024-10-26 16:29:36 +0200

    RPI RF emulation capacity demo

--- apps/Sub-GHz/RPImodulation/__init__.py
+++ apps/Sub-GHz/RPImodulation/__init__.py
@@ -2,7 +2,7 @@ from PiPerW.interfaces.app_interface import AppInterface
 from PiPerW.pheripherals import Pheripherals
 from PiPerW.display import Display
 from PiPerW.helpers import Log, Config, download_lib_from_github, select_number
-from PiPerW.utils.Menu import Menu
+from PiPerW.utils.Menu import Menu, MenuFromDataFolder
 import os, sys, time
 import subprocess
 
@@ -31,6 +31,9 @@ class App(AppInterface):
         self.executable_root= self.root_path +'/'+ self.lib_path
         self.frequency = 433.00
         self.process = None
+        
+        display.text("This app is to test RPI RF emulation capabilities")
+        time.sleep(2)
         
         # check if PiFmRds is installed 
         if not os.path.exists(self.lib_path) or not os.path.exists(self.lib_path + "/spectrumpaint"):
@@ -102,7 +105,8 @@ class App(AppInterface):
             "Carrier",  
             "Chirp",
             "Spectrum",
-            "SSB",
+            "AM audio",
+            "SendIQ"
         ]
         
         menu = Menu(options)
@@ -120,35 +124,6 @@ class App(AppInterface):
             elif key == "back" or key == "exit":
                 break
             
-    
-    
-    def select_frequency(self):
-        '''
-        Select frequency to play music
-        '''
-        
-        display.text("Move to change frequency\nPress select to confirm")
-        multiplier = 10
-        while True:
-            key = pheripherals.await_key()
-            text = "Select frequency\n"
-            if key == "right":
-                self.frequency += (0.01 * multiplier)
-            elif key == "left":
-                self.frequency -= (0.01 * multiplier)
-            elif key == "up":
-                multiplier = multiplier * 10 if multiplier < 1000 else 1000
-            elif key == "down":
-                multiplier = multiplier/ 10 if multiplier > 1 else 1
-            elif key == "select":
-                break
-            elif key == "back":
-                sys.exit(0)
-            
-            self.frequency = round(self.frequency, 2)
-            text += "Frequency: {:.2f} MHz\n".format(self.frequency)
-            text += "Multiplier: {}\n ↕ to modify".format(multiplier)
-            display.text(text)
     
     
     def start_process(self, executable, args):
@@ -190,8 +165,10 @@ class App(AppInterface):
             self.play_chirp()
         elif option == "Spectrum":
             self.play_spectrum()
-        elif option == "SSB":
-            self.play_ssb()
+        elif option == "AM audio":
+            self.play_am_audio()
+        elif option == "SendIQ":
+            self.send_iq()
     
     
     def play_carrier(self):
@@ -199,7 +176,8 @@ class App(AppInterface):
         Play carrier wave
         '''
         display.text("Select frequency to generate the carier music")
-        self.select_frequency()
+        self.frequency = select_number(display, pheripherals, start_number=self.frequency, title="Select frequency", decimals = 2,  min=400, max=500)
+
         
         display.text("Playing carrier wave\nPress any key to stop")
         Log.info("Playing carrier wave with frequency: "+str(self.frequency))
@@ -221,7 +199,9 @@ class App(AppInterface):
         Play chirp wave
         '''
         display.text("Select frequency to generate the chirp music")
-        self.select_frequency()
+        # self.select_frequency()
+        self.frequency = select_number(display, pheripherals, start_number=self.frequency, title="Select frequency", decimals = 2,  min=400, max=500)
+
         
         bandwith = select_number(display, pheripherals, title="Select bandwith", scale=100,  min=50000, max=100000)
         
@@ -238,3 +218,74 @@ class App(AppInterface):
         
         self.stop_process()
         
+    def play_spectrum(self):
+        ''' 
+        Show a spectrum image
+        '''
+        display.text("Select frequency to generate the spectrum")
+        self.frequency = select_number(display, pheripherals, start_number=self.frequency, title="Select frequency", decimals = 2,  min=400, max=500)
+        
+        display.text("Select image to show")
+        # png, jpg, bmp, etc regex pattern
+        image_menu = MenuFromDataFolder("Sub-GHz/RPImodulation", type_items="file", pattern=".*\.(png|jpg|bmp)")
+        image = image_menu.choose()
+        image =image_menu.get_full_path()
+        
+        display.text("Showing spectrum image at frequency: "+str(self.frequency)+" MHz\nPress any key to stop")
+        # pillow convert image to 320x256        
+        os.system(f"convert {image} -resize 320x256 -flip -quantize YUV -dither FloydSteinberg -colors 4 \-interlace partition /tmp/spectrum.yuv")
+        self.start_process(self.executable_root+"/spectrumpaint", ["/tmp/spectrum.yuv", str(self.frequency)+"e6", "100000"])
+        pheripherals.await_any_key_press()
+        self.stop_process()
+        
+    def play_am_audio(self):
+        '''
+        Play AM audio
+        '''
+        display.text("Select frequency to generate the AM audio")
+        self.frequency = select_number(display, pheripherals, start_number=self.frequency, title="Select frequency", decimals = 2,  min=400, max=500)
+        
+        display.text("Select audio file to play\nFile: wav, mp3, etc in MONO")
+        # wav, mp3, etc regex pattern
+        audio_menu = MenuFromDataFolder("Sub-GHz/RPImodulation", type_items="file", pattern=".*\.(wav|mp3)")
+        audio_file = audio_menu.choose()
+        audio_file = audio_menu.get_full_path()
+        
+        display.text("Playing AM audio at frequency: "+str(self.frequency)+" MHz\nPress any key to stop")
+        Log.info("Playing AM audio at frequency: "+str(self.frequency)+" MHz")
+        
+        # comand to execute
+        # (while true; do cat "$2"; done) | csdr convert_i16_f \
+#   | csdr gain_ff 4.0 | csdr dsb_fc \
+#   | sudo ./rpitx -i - -m IQFLOAT -f "$1" -s 48000
+
+        self.start_process("bash", ["-c", f"(while true; do cat '{audio_file}'; done) | csdr convert_i16_f | csdr gain_ff 4.0 | csdr dsb_fc | {self.executable_root}/sendiq -s 48000 -f {self.frequency}e6 -t i16 -i -"])
+        pheripherals.await_any_key_press()
+        self.stop_process()
+        
+        
+    def send_iq(self):
+        '''
+        Send IQ data
+        '''
+        
+        display.text("Select frequency to send the IQ file")
+        self.frequency = select_number(display, pheripherals, start_number=self.frequency, title="Select frequency", decimals = 2,  min=400, max=500)
+        
+        display.text("Select IQ file to send\nFile: /816bit, sample rate 10000-250000")
+        Log.warning("FORMAT FILE: *-(u8|i16)-[0-9]+.(wav|iq)  -> example: myiq-i16-10000.iq, myiq-u8-250000.wav")
+        # wav, iq, etc regex pattern with name, format and sample rate
+        iq_menu = MenuFromDataFolder("Sub-GHz/RPImodulation", type_items="file", pattern=".*-(u8|i16)-[0-9]+.(wav|iq)")
+        iq_file = iq_menu.choose()
+        iq_file = iq_menu.get_full_path()
+        
+        display.text("Sending IQ file at frequency: "+str(self.frequency)+" MHz\nPress any key to stop")
+        Log.info("Sending IQ file at frequency: "+str(self.frequency)+" MHz")
+        
+        # get type and sample rate
+        tipe_file, sample_rate = iq_file.split("-")[-2:]
+        sample_rate = sample_rate.split(".")[0]
+        
+        self.start_process(self.executable_root+"/sendiq", ["-s", sample_rate, "-f", str(self.frequency)+"e6", "-t", tipe_file, "-i", iq_file])
+        pheripherals.await_any_key_press()
+        self.stop_process()
